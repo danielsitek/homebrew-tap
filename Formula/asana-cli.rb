227920ff702025c29df9a751a27b29243aca2663
Daniel Sitek <dan.sitek@gmail.com>
2026-08-03 15:37:36 +0200

    feat: update asana-cli to 0.2.0

--- Formula/asana-cli.rb
+++ Formula/asana-cli.rb
@@ -5,17 +5,18 @@ class AsanaCli < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/danielsitek/asana-cli/releases/download/v0.1.0/asana-cli-v0.1.0-darwin-arm64.tar.gz"
-      sha256 "073285b09b07693d9799259abe163d1121b7b17c98e8e4ddd1f52b1ca3ea85de"
+      url "https://github.com/danielsitek/asana-cli/releases/download/v0.2.0/asana-cli-v0.2.0-darwin-arm64.tar.gz"
+      sha256 "279529495d4d1219859efd5d8e183bda5ecc1dd02b63e3e2363d94ed6b42117b"
     end
     on_intel do
-      url "https://github.com/danielsitek/asana-cli/releases/download/v0.1.0/asana-cli-v0.1.0-darwin-x64.tar.gz"
-      sha256 "c00183bb647e7855e386d85af15719a973ed46274e1f2c85d68c07161a421d9d"
+      url "https://github.com/danielsitek/asana-cli/releases/download/v0.2.0/asana-cli-v0.2.0-darwin-x64.tar.gz"
+      sha256 "2973052e88caafaa4360c4beaf3058289eaec573785c22db0008ab9f5d310959"
     end
   end
 
   def install
     bin.install "asana-cli"
+    generate_completions_from_executable bin/"asana-cli", "completion"
   end
 
   test do
